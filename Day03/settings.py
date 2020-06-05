86c08ec505055cc6d858ce7b1711d7f1c2dfa5d3
unknown <dnjseka1@khu.ac.kr>
2020-06-05 20:20:46 +0900

    new2

--- Day03/settings.py
+++ Day03/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'tqx82)txto9p+-bhz2cpsed_23kb1l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
